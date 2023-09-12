11c8e7830b1530aaeac246c0889a282a352f6226
fionafrm <fiona.ratu@ui.ac.id>
2023-09-12 09:16:44 +0700

    Mencoba menambahkan footer pada main

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_main(request):
         'name': 'Sweet Madame',
         'desc': 'Depending on the quality, Sweet Madame restores 20/22/24% of Max HP and an additional 900/1,200/1,500 HP to the target character. Like most foods, this cannot target other players' + ' characters in Co-Op Mode. When Rosaria cooks Sweet Madame, there is a chance that Dinner of Judgment will be created instead.',
         'amt' : '150',
-        'category' : 'food'
+        'category' : 'Food'
     }
 
     return render(request, "main.html", context)
